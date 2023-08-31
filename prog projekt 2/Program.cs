65afb4833d4464b7ef671d3db291ca607766462e
Elias Saarenala <elias.saarenala@elev.ga.ntig.se>
2023-08-31 10:26:59 +0200

    första comit

---
+++ prog projekt 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Vilken mat tycker du om?");
+string mat = Console.ReadLine();
+
+Console.WriteLine($"Vilket spel tycker du om?");
+string spel = Console.ReadLine();
+
+ Console.WriteLine($"Så {name} tycker om att äta {mat} och spela {spel}" );
+
+Console.WriteLine($"tycker du om mat?");
+ if Console.ReadLine =  { 
+
+
+ }
+
+Console.ReadLine();
